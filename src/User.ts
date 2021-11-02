8cd1fab922a2a613ea1a8bed2d0cababa48d6ea1
Omar-Said <omar.a.said@outlook.com>
2021-11-02 10:44:44 -0400

    created base user object

--- src/User.ts
+++ src/User.ts
@@ -7,5 +7,11 @@ class User {
     lng: number;
   };
 
-  constructor() {}
+  constructor() {
+    this.name = faker.name.findName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
 }
